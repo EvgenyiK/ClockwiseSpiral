cc246d658f7dd3cd75ec145afed89043c2035ac4
Evgen <evgenprogrammer@gmail.com>
2022-02-26 22:09:17 +0300

    add client

---
+++ client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	sp "github.com/EvgenyiK/ClockwiseSpiral/proto"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, _ := grpc.Dial("127.0.0.1:8080",grpc.WithInsecure())
+
+	client := sp.NewClockWiseSpiralServiceClient(conn)
+
+	var a int
+	flag.IntVar(&a, "a", 0, "The integer param")
+	flag.Parse()
+
+
+	resp, err := client.GenerateSpiral(context.Background(), &sp.NumbRequest{Num: int32(a)})
+	if err != nil {
+		logrus.Fatalf("could not get answer: %v", err)
+	}
+
+	for _, v := range resp.Responses {
+		fmt.Println(v.Mid)
+	}
+}
